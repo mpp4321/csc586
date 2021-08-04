90139d221a382ddd24b1770c5da8816274f916ff
mpp4321 <47093830+mpp4321@users.noreply.github.com>
2021-08-03 21:45:17 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -19,21 +19,17 @@ request.addTour(tour)
 prefixForIP = "192.168.1."
 link = request.LAN("lan")
 
-# Add a raw PC to the request.
-node1 = request.XenVM("webserver")
-node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-node1.routable_control_ip = "true"
-
-# Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
-
-node = request.XenVM("observer")
-node.routable_control_ip = "true"  
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-iface = node.addInterface("if0")
-iface.component_id = "eth1"
-iface.addAddress(pg.IPv4Address(prefixForIP + str(1), "255.255.255.0"))
-link.addInterface(iface)
-
+for i in range(2):
+  if i == 0:
+    node = request.XenVM("webserver")
+    node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+  elif i == 1:
+    node = request.XenVM("observer")
+  node.routable_control_ip = "true"  
+  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+  iface = node.addInterface("if" + str(i))
+  iface.component_id = "eth1"
+  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
+  link.addInterface(iface)  
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
